e7a2ce8d171ff32ff4b503698a6c2fd473590706
Justin Spies <justin@thespies.org>
2018-01-26 12:48:45 -0500

    Add Papertrail tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # system-baseline Inspec Tests CHANGELOG
 
+## 1.1.0
+- Add tests for proper configuration of Papertrail remote syslog
+
 ## 1.0.1
 - Allow overriding the default domain name for cases where a custom domain is not specified for the Chef attributes in
 `.kitchen.yml`
---
+++ controls/logging.rb
@@ -0,0 +1,50 @@
+# Copyright (C) 2016 - 2017 Justin Spies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+papertrail_host = attribute('papertrail_host', default: nil,
+                            description: 'Hostname to be compred when testing that papertrail is properly configured')
+
+control 'papertrail' do
+  impact 1.0
+  title 'Papertrail audit configuration'
+  desc 'Provides controls to ensure that papertrail remote syslog configuration is accurate'
+
+  describe package('rsyslog-gnutls') do
+    it { should be_installed }
+  end
+
+  describe file('/var/spool/rsyslog') do
+    it { should be_director }
+    it { should be_owned_by('root') }
+    it { should be_grouped_into('root') }
+    its('mode') { should cmp '0770' }
+  end
+
+  describe file('/etc/papertrail-bundle.pem') do
+    it { should be_director }
+    it { should be_owned_by('root') }
+    it { should be_grouped_into('root') }
+    its('mode') { should cmp '0644' }
+  end
+
+  describe file('/etc/rsyslog.d/22-papertrail.conf') do
+    it { should be_director }
+    it { should be_owned_by('root') }
+    it { should be_grouped_into('root') }
+    its('mode') { should cmp '0644' }
+    its(:content) { should match(/\*\.\*\s*#{papertrail_host}$/i) }
+  end
+
+  not_if { papertrail_host.nil? }
+end
