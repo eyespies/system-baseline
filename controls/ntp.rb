7707d0734bced83e08aba2444069c5b985f3039e
Justin Spies <justin@thespies.org>
2018-02-01 00:25:39 -0500

    Allow variations for the listening IP and protocols

--- controls/ntp.rb
+++ controls/ntp.rb
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+localhost_only = attribute('listen_localhost_only', default: false, description: 'If true, NTP should only listen on localhost and not expose the serivce to outside systems')
+disable_ipv4 = attribute('disable_ipv4', default: false, description: 'If true, NTP should not listen on an IPv4 address')
+disable_ipv6 = attribute('disable_ipv6', default: false, description: 'If true, NTP should not listen on an IPv6 address')
+
 control 'ntp-services' do
   impact 1.0
   title 'Network Time Protocol Service'
@@ -31,8 +35,25 @@ control 'ntp-services' do
   # TODO: Can we do listening on specific IPs?
   # TODO: Add negative testing for things that should NOT be listening
   describe port('123') do
-    its(:protocols) { should include('udp') }
-    its(:addresses) { should include('0.0.0.0') }
+    if disable_ipv4
+      its(:protocols) { should_not include('udp') }
+    else
+      its(:protocols) { should include('udp') }
+    end
+
+    if disable_ipv6
+      its(:protocols) { should_not include('udp6') }
+    else
+      its(:protocols) { should include('udp6') }
+    end
+
+    if localhost_only
+      its(:addresses) { should include('127.0.0.1') }
+      its(:addresses) { should_not include('0.0.0.0') }
+    else
+      its(:addresses) { should_not include('127.0.0.1') }
+      its(:addresses) { should include('0.0.0.0') }
+    end
   end
 
   # ~ Service Checks ~ #
