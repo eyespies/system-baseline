fbe813995ca756f410f2473c53967ae23ae3b020
Justin Spies <justin@thespies.org>
2020-03-25 21:15:27 -0400

    (fix) use new "value" instead of "default"; support RHEL platforms less than 8 since ntpd is removed in RHEL8 and higher

--- controls/ntp.rb
+++ controls/ntp.rb
@@ -14,52 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-localhost_only = attribute('listen_localhost_only', default: 'false', description: 'If true, NTP should only listen on localhost and not expose the serivce to outside systems')
+localhost_only = attribute('listen_localhost_only',
+                           value: 'false',
+                           description: 'If true, NTP should only listen on localhost and not expose the serivce to outside systems')
 
-control 'ntp-services' do
-  impact 1.0
-  title 'Network Time Protocol Service'
-  desc 'TBD'
+if %i[centos redhat oracle].include?(os[:name]) && os[:release] =~ /^8/
+  control 'ntp-services' do
+    impact 1.0
+    title 'Network Time Protocol Service'
+    desc 'Validates the installation and configuration of the NTP service'
 
-  # TODO: What about /etc/ntp/keys and /etc/ntp/step-tickers?
-  describe file('/etc/ntp.conf') do
-    it { should exist }
-    it { should be_owned_by('root') }
-    it { should be_grouped_into('root') }
-    its('mode') { should cmp '0644' }
-    its(:content) { should match(/server [0-9].pool.ntp.org iburst minpoll [0-9] maxpoll [0-9]*/i) }
-    its(:content) { should match(/restrict [0-9].pool.ntp.org nomodify notrap noquery/i) }
-  end
-
-  # ~ Listening Ports Checks ~ #
-  # TODO: Can we do listening on specific IPs?
-  # TODO: Add negative testing for things that should NOT be listening
-  describe port('123') do
-    its(:addresses) { should include('127.0.0.1') }
-    if localhost_only == 'true'
-      its(:addresses) { should_not include('0.0.0.0') }
-    else
-      its(:addresses) { should include('0.0.0.0') }
+    # TODO: What about /etc/ntp/keys and /etc/ntp/step-tickers?
+    describe file('/etc/ntp.conf') do
+      it { should exist }
+      it { should be_owned_by('root') }
+      it { should be_grouped_into('root') }
+      its('mode') { should cmp '0644' }
+      its(:content) { should match(/server [0-9].pool.ntp.org iburst minpoll [0-9] maxpoll [0-9]*/i) }
+      its(:content) { should match(/restrict [0-9].pool.ntp.org nomodify notrap noquery/i) }
     end
-  end
 
-  # ~ Service Checks ~ #
-  ntp_service = case os.name
-                when 'centos', 'redhat', 'oracle'
-                  'ntpd'
-                when 'debian', 'ubuntu'
-                  'ntp'
-                end
+    # ~ Listening Ports Checks ~ #
+    # TODO: Can we do listening on specific IPs?
+    # TODO: Add negative testing for things that should NOT be listening
+    describe port('123') do
+      its(:addresses) { should include('127.0.0.1') }
+      if localhost_only == 'true'
+        its(:addresses) { should_not include('0.0.0.0') }
+      else
+        its(:addresses) { should include('0.0.0.0') }
+      end
+    end
 
-  describe service(ntp_service) do
-    it { should be_enabled }
-    it { should be_running }
-  end
+    # ~ Service Checks ~ #
+    ntp_service = case os.name
+                  when 'centos', 'redhat', 'oracle'
+                    'ntpd'
+                  when 'debian', 'ubuntu'
+                    'ntp'
+                  end
 
-  # ~ Process Checks ~ #
-  describe processes('ntpd') do
-    # Not currently working for somer reason.
-    its('states') { should include('Ss') }
-    its('entries.length') { should eq 1 }
+    describe service(ntp_service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    # ~ Process Checks ~ #
+    describe processes('ntpd') do
+      # Not currently working for somer reason.
+      its('states') { should include('Ss') }
+      its('entries.length') { should eq 1 }
+    end
   end
 end
